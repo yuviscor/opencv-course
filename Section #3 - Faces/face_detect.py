c4b97f63b96c5d186dc3e7056e64d22a69c385e1
Yuvraj <66749683+yuviscor@users.noreply.github.com>
2021-07-15 23:57:30 +0530

    Update face_detect.py

--- Section #3 - Faces/face_detect.py
+++ Section #3 - Faces/face_detect.py
@@ -22,3 +22,36 @@ cv.imshow('Detected Faces', img)
 
 
 cv.waitKey(0)
+
+
+[------------------------------------------------------------]
+#for video detection
+
+import cv2 as c
+
+
+cap = c.VideoCapture(0)
+
+harr = c.CascadeClassifier('haar_face.xml')
+while True:
+    isT, frame = cap.read()
+
+    gray  = c.cvtColor(frame,c.COLOR_RGB2GRAY)
+    
+    face = harr.detectMultiScale(gray,scaleFactor=1.9,minNeighbors=1)
+
+    for x,y,w,h in face:
+        c.rectangle(frame,(x,y),(x+w,y+h),(222,222,222),thickness=2)
+
+        r = gray[y:y+h,x:x+w]
+        rc = frame[y:y+h,x:x+w]
+
+    c.imshow('v', frame)
+   
+
+    if(c.waitKey(2) & 0xFF == ord('d')):
+        break
+
+cap.release()
+
+c.destroyAllWindows()
